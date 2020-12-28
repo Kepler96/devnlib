cba936eb4b000dbff43a34b986e9cfb7676d665b
Kepler96 <74507600+Kepler96@users.noreply.github.com>
2020-12-28 23:08:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// const express = require('express');
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 4000;
 const nav=[
     {
         link:'/books',
@@ -46,23 +42,14 @@ app.use('/updatebook',updatebookRouter);
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
 
-var bodyParser = require('body-parser');
-const session = require('express-session');
-const { Router } = require('express');
+
 //const session = require('session');
 // var app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.bodyParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.bodyParser());
+
 // Add this for session
 // app.use(session({
 //   secret: 'password',//add secret key
@@ -76,21 +63,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-app.listen(4000);
+app.get("/",function(req,res){
+  res.render("index",
+      {
+         nav,
+          title:'Library',
+          heading:'3DS Library'
+      }
+  );
+  });
+   
+  app.listen(port,()=>{console.log("server ready at  "+port);});
